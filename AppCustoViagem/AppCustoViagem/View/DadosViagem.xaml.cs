ce21226cb4f0a9c32331412f971863a1159c0cc2
Usuario <Usuario@CPD03-M05>
2022-09-13 11:44:39 -0300

    mudando as cores

--- AppCustoViagem/AppCustoViagem/View/DadosViagem.xaml.cs
+++ AppCustoViagem/AppCustoViagem/View/DadosViagem.xaml.cs
@@ -19,6 +19,12 @@ namespace AppCustoViagem.View
 
         private void ToolbarItem_Clicked(object sender, EventArgs e)
         {
+            Navigation.PushAsync(new Listagem());
+        }
+
+        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
+        {
+            
             double distancia = Convert.ToDouble(txt_distancia.Text);
             double preco_combustivel = Convert.ToDouble(txt_preco_combustivel.Text);
             double km_litro = Convert.ToDouble(txt_km_litro.Text);
@@ -35,11 +41,6 @@ namespace AppCustoViagem.View
             spn_custo_combustivel.Text = custo_combustivel.ToString("C");
             spn_custo_pedagios.Text = custo_pedagio.ToString("C");
             lbl_custo_viagem.Text = custo_viagem.ToString("C");
-        }
-
-        private void ToolbarItem_Clicked_1(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new Listagem());
         }
 
        
